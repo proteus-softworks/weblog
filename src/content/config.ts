7f5a2ecade38dd03ce004928bc2be8e179ac7e38
Brandon Wetzel <mauzybwy@gmail.com>
2025-12-07 22:32:11 -0500

    Use obsidian posts in blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,11 +14,24 @@ const postsCollection = defineCollection({
     }),
 });
 
+const obsidianPostsCollection = defineCollection({
+  loader: glob({ base: "./src/content/weblog-docs", pattern: "**/*.md" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+    }),
+});
+
 const recipesCollection = defineCollection({
   loader: cookLoader({ base: "./src/content/recipes" }),
 });
 
 export const collections = {
   posts: postsCollection,
+  obsidianPosts: obsidianPostsCollection,
   recipes: recipesCollection,
 };
