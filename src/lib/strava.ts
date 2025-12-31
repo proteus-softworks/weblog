1cf5497d564d5e9f30c0e04cbcee7ddd2c267969
Brandon Wetzel <mauzybwy@gmail.com>
2025-12-31 13:34:35 -0500

    Cache strava stuff

--- src/lib/strava.ts
+++ src/lib/strava.ts
@@ -1,25 +1,63 @@
-export async function getStats() {
-  const response = await fetch("https://www.strava.com/oauth/token", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      client_id: process.env.STRAVA_CLIENT_ID,
-      client_secret: process.env.STRAVA_CLIENT_SECRET,
-      refresh_token: process.env.STRAVA_REFRESH_TOKEN,
-      grant_type: "refresh_token",
-    }),
-  });
+/******************************************************************************
+ * Import
+ ******************************************************************************/
+import Axios from "axios";
+import { setupCache } from "axios-cache-interceptor";
 
-  const data = await response.json();
+/******************************************************************************
+ * Axios Config
+ ******************************************************************************/
 
-  const statsResponse = await fetch(
-    "https://www.strava.com/api/v3/athletes/6765104/stats",
-    {
+const instance = Axios.create({
+  baseURL: "https://www.strava.com/api/v3",
+});
+
+const axios = setupCache(instance, {
+  enabled: true,
+  methods: ["get", "post", "head"],
+  // Ignore all headers :)
+  headerInterpreter: () => "not enough headers",
+});
+
+/******************************************************************************
+ * Public Functions
+ ******************************************************************************/
+
+export const StravaGateway = {
+  async getStats() {
+    const accessToken = await getAccessToken();
+
+    const response = await axios.get("/athletes/6765104/stats", {
       headers: {
-        Authorization: `Bearer ${data.access_token}`,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return response.data;
+  },
+};
+
+/******************************************************************************
+ * Private Functions
+ ******************************************************************************/
+
+async function getAccessToken() {
+  const response = await axios.post(
+    "oauth/token",
+    {},
+    {
+      headers: { "Content-Type": "application/json" },
+      params: {
+        client_id: process.env.STRAVA_CLIENT_ID,
+        client_secret: process.env.STRAVA_CLIENT_SECRET,
+        refresh_token: process.env.STRAVA_REFRESH_TOKEN,
+        grant_type: "refresh_token",
+      },
+      cache: {
+        ttl: 1000 * 60, // 1 minute.
       },
     },
   );
 
-  return await statsResponse.json();
+  return response.data.access_token;
 }
